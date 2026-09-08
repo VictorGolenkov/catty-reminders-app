96847b9b38a7dc2594e2d569648afcdd9827b2bb
VictorGolenkov <victorgolenkov65@gmail.com>
2026-09-08 13:49:49 +0400

    test

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,7 @@ if [ -f "requirements.txt" ]; then
 fi
 
 # 3. Перезапускаем приложение через systemd
-COMMIT_HASH=$(git rev-parse --short HEAD)
+COMMIT_HASH=$(git rev-parse HEAD)
 echo "Код коммита: $COMMIT_HASH"
 echo "DEPLOY_REF=$COMMIT_HASH" > .env
 sudo /usr/bin/systemctl restart catty-app
